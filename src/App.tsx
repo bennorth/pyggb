cb461b8e596580054c9027c84a4cde0c922db2e8
Ben North <ben@redfrontdoor.org>
2022-12-14 15:15:07 +0000

    App: Use IDE

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import React from "react";
 import "./App.css";
+import { IDE } from "./components/IDE";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IDE />
     </div>
   );
 }
