ff7700ef95b35c4520d1bd71c3c52d89b35e64b3
Ben North <ben@redfrontdoor.org>
2022-12-16 15:23:07 +0000

    Boot dependencies on App render

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { IDE } from "./components/IDE";
+import { useStoreActions } from "./store";
 
 function App() {
+  const bootDependencies = useStoreActions((a) => a.dependencies.boot);
+
+  useEffect(() => {
+    bootDependencies();
+  });
+
   return (
     <div className="App">
       <IDE />
