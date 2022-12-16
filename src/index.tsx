9bb32901ecb5d8b75f52f200b9260f70cd6bd415
Ben North <ben@redfrontdoor.org>
2022-12-16 13:25:01 +0000

    (Reformat)

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import { store } from "./store";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <StoreProvider store={store}>
