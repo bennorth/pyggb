75a8c61b89635c4b561f14a9b7c3206651af989f
Ben North <ben@redfrontdoor.org>
2022-12-16 15:25:15 +0000

    Controls: Add to top-level model

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,12 +1,15 @@
 import { Editor, editor } from "./editor";
+import { Controls, controls } from "./controls";
 import { Dependencies, dependencies } from "./dependencies";
 
 export type PyGgbModel = {
   editor: Editor;
+  controls: Controls;
   dependencies: Dependencies;
 };
 
 export const pyGgbModel: PyGgbModel = {
   editor,
+  controls,
   dependencies,
 };
