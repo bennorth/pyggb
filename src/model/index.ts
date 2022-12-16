bffe2a45ad879f8e9e35b63be644762eda22d4f0
Ben North <ben@redfrontdoor.org>
2022-12-16 15:22:11 +0000

    Dependencies: Add to top-level model

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,9 +1,12 @@
 import { Editor, editor } from "./editor";
+import { Dependencies, dependencies } from "./dependencies";
 
 export type PyGgbModel = {
   editor: Editor;
+  dependencies: Dependencies;
 };
 
 export const pyGgbModel: PyGgbModel = {
   editor,
+  dependencies,
 };
