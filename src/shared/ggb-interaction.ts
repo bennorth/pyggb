76da5822ea8bdab79be08e6d37b2cb107a664ec5
Ben North <ben@redfrontdoor.org>
2022-12-16 15:20:14 +0000

    $ggbApiHandoverQueue: Define interface object

--- src/shared/ggb-interaction.ts
+++ src/shared/ggb-interaction.ts
@@ -1,2 +1,19 @@
 // TODO: Replace this with proper type:
 export type GgbApi = any;
+
+(globalThis as any).$ggbApiHandoverQueue = (() => {
+  let queue: Array<GgbApi> = [];
+
+  const enqueue = (api: GgbApi): void => {
+    queue.push(api);
+  };
+
+  const dequeue = (): GgbApi => {
+    const api = queue.shift();
+    if (api == null) console.error("api queue empty!");
+
+    return api;
+  };
+
+  return { enqueue, dequeue };
+})();
