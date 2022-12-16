231e85fe6e98fd3e032370411e8ba445e64400ed
Ben North <ben@redfrontdoor.org>
2022-12-16 15:13:27 +0000

    Dependencies.ggbPythonModuleText: Add
    Also setter action.

--- src/model/dependencies.ts
+++ src/model/dependencies.ts
@@ -1,9 +1,19 @@
+import { action, Action, computed, Computed, thunk, Thunk } from "easy-peasy";
+
 type BootStatus = "idle" | "running" | "done";
 
 export type Dependencies = {
   bootStatus: BootStatus;
+  ggbPythonModuleText: string;
+
+  setGgbPythonModuleText: Action<Dependencies, string>;
 };
 
 export const dependencies: Dependencies = {
   bootStatus: "idle",
+  ggbPythonModuleText: "",
+
+  setGgbPythonModuleText: action((s, moduleText) => {
+    s.ggbPythonModuleText = moduleText;
+  }),
 };
