473c9c0483c82bf5db592e392188eeda2d47d9c8
Ben North <ben@redfrontdoor.org>
2022-12-16 15:13:58 +0000

    Dependencies.ggbApi: Add
    Also setter action.

--- src/model/dependencies.ts
+++ src/model/dependencies.ts
@@ -4,15 +4,21 @@ type BootStatus = "idle" | "running" | "done";
 
 export type Dependencies = {
   bootStatus: BootStatus;
+  ggbApi: any;
   ggbPythonModuleText: string;
 
+  setGgbApi: Action<Dependencies, any>;
   setGgbPythonModuleText: Action<Dependencies, string>;
 };
 
 export const dependencies: Dependencies = {
   bootStatus: "idle",
+  ggbApi: null,
   ggbPythonModuleText: "",
 
+  setGgbApi: action((s, api) => {
+    s.ggbApi = api;
+  }),
   setGgbPythonModuleText: action((s, moduleText) => {
     s.ggbPythonModuleText = moduleText;
   }),
