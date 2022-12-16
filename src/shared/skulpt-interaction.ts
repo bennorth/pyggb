c99abe00a4e9bba89855a7e387102455716b845a
Ben North <ben@redfrontdoor.org>
2022-12-16 13:22:48 +0000

    builtinRead(): Add

--- src/shared/skulpt-interaction.ts
+++ src/shared/skulpt-interaction.ts
@@ -1,1 +1,13 @@
 declare var Sk: any;
+
+const builtinRead = (filename: string) => {
+  console.log(`builtinRead("${filename}")`);
+
+  if (
+    Sk.builtinFiles === undefined ||
+    Sk.builtinFiles["files"][filename] === undefined
+  )
+    throw `File not found: "${filename}"`;
+
+  return Sk.builtinFiles["files"][filename];
+};
