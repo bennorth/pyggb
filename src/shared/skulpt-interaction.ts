ca0090afb2f739d7d0a64dce6587f8c0fda3c3ed
Ben North <ben@redfrontdoor.org>
2022-12-16 13:22:56 +0000

    runPythonProgram(): Add

--- src/shared/skulpt-interaction.ts
+++ src/shared/skulpt-interaction.ts
@@ -11,3 +11,17 @@ const builtinRead = (filename: string) => {
 
   return Sk.builtinFiles["files"][filename];
 };
+
+export const runPythonProgram = (codeText: string) => {
+  Sk.configure({
+    output: (s: string) => console.log(s),
+    read: builtinRead,
+    __future__: Sk.python3,
+    inputfun: (promptText: string) => prompt(promptText),
+    inputfunTakesPrompt: true /* then you need to output the prompt yourself */,
+  });
+
+  Sk.misceval.asyncToPromise(function () {
+    return Sk.importMainWithBody("<stdin>", false, codeText, true);
+  });
+};
