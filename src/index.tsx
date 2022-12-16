8b2eac32facb74e16c2f3cd07966dadeb84d7e77
Ben North <ben@redfrontdoor.org>
2022-12-16 13:24:20 +0000

    Run app in context of StoreProvider

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./pyggb.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { StoreProvider } from "easy-peasy";
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <StoreProvider store={store}>
+      <App />
+    </StoreProvider>
   </React.StrictMode>
 );
 
