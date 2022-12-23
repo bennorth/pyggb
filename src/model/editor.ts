aa095dad5f238150eab262b2a71d3880da1e5b2d
Ben North <ben@redfrontdoor.org>
2022-12-23 15:02:29 +0000

    Editor.setCodeText(): Initialise seqnum

--- src/model/editor.ts
+++ src/model/editor.ts
@@ -18,12 +18,15 @@ export type Editor = {
   setBackingFileState: Action<Editor, BackingFileState>;
 };
 
+const InitialCodeTextSeqNum = 1001;
+
 export const editor: Editor = {
   backingFileState: { status: "booting" },
   codeText: "",
   codeTextSeqNum: 0,
   setCodeText: action((s, codeText) => {
     s.codeText = codeText;
+    s.codeTextSeqNum = InitialCodeTextSeqNum;
   }),
   setBackingFileState: action((s, state) => {
     s.backingFileState = state;
