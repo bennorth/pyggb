ea4449cd400952aa498af083513406065744db17
Ben North <ben@redfrontdoor.org>
2022-12-23 15:28:03 +0000

    NewFileModal: Add state for name
    Disable Create button when name empty.

--- src/components/modals/NewFileModal.tsx
+++ src/components/modals/NewFileModal.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
 import { useStoreActions, useStoreState } from "../../store";
 
 export const NewFileModal: React.FC<{}> = () => {
   const active = useStoreState((s) => s.modals.newFile.active);
   const setActive = useStoreActions((a) => a.modals.newFile.setActive);
+
+  const [newName, setNewName] = useState("");
+  const createEnabled = newName !== "";
 
   const dismiss = () => setActive(false);
 
@@ -15,14 +18,22 @@ export const NewFileModal: React.FC<{}> = () => {
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Control type="text"></Form.Control>
+          <Form.Control
+            type="text"
+            value={newName}
+            onChange={(e) => {
+              setNewName(e.target.value);
+            }}
+          ></Form.Control>
         </Form>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={dismiss}>
           Cancel
         </Button>
-        <Button variant="primary">Create</Button>
+        <Button disabled={!createEnabled} variant="primary">
+          Create
+        </Button>
       </Modal.Footer>
     </Modal>
   );
