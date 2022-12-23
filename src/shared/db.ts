f9c147c40ef27ebf74832511ac5e586ea166be3d
Ben North <ben@redfrontdoor.org>
2022-12-23 12:51:34 +0000

    UserFile: Define type and friends

--- src/shared/db.ts
+++ src/shared/db.ts
@@ -1,1 +1,11 @@
 import Dexie, { Table } from "dexie";
+
+export type UserFile = {
+  id?: number;
+  name: string;
+  codeText: string;
+  mtime: number;
+};
+
+export type UserFileUpdate = Required<Pick<UserFile, "id" | "codeText">>;
+export type UserFilePreview = Required<Pick<UserFile, "id" | "name">>;
