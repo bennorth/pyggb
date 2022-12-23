721ac8813a024b9c391517ea20ec36c4b7b4de31
Ben North <ben@redfrontdoor.org>
2022-12-23 15:09:23 +0000

    MenuBar: Add indication of loading/saving state

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,8 +1,19 @@
+import classnames from "classnames";
 import React from "react";
-import { Container, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Navbar, NavDropdown, Spinner } from "react-bootstrap";
+import { useStoreActions, useStoreState } from "../store";
 import { RunButton } from "./RunButton";
 
 export const MenuBar: React.FC<{}> = () => {
+  const backingState = useStoreState((s) => s.editor.backingFileState);
+
+  const spinnerClasses = classnames({
+    visible: backingState.status !== "idle",
+  });
+
+  const maybeBackingName =
+    backingState.status === "booting" ? null : backingState.name;
+
   return (
     <Navbar>
       <Container fluid className="MenuBar">
@@ -16,6 +27,10 @@ export const MenuBar: React.FC<{}> = () => {
           <NavDropdown.Item>Make a copy</NavDropdown.Item>
           <NavDropdown.Item>Download</NavDropdown.Item>
         </NavDropdown>
+        <Navbar.Text className="backing-state">
+          <Spinner size="sm" className={spinnerClasses}></Spinner>
+          {maybeBackingName}
+        </Navbar.Text>
       </Container>
     </Navbar>
   );
