b3ef72152deb330a1e88dc6afff2510f9e53ede0
Ben North <ben@redfrontdoor.org>
2022-12-23 09:39:44 +0000

    Remove reportWebVitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./pyggb.scss";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { StoreProvider } from "easy-peasy";
 import { store } from "./store";
 import "./shared/ggb-interaction";
@@ -18,8 +17,3 @@ root.render(
     </StoreProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
