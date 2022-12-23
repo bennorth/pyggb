b6bb927f4ed48213a4f069bc88d3894c9d45950e
Ben North <ben@redfrontdoor.org>
2022-12-23 15:11:30 +0000

    Launch modals from "File" menu items

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -6,6 +6,13 @@ import { RunButton } from "./RunButton";
 
 export const MenuBar: React.FC<{}> = () => {
   const backingState = useStoreState((s) => s.editor.backingFileState);
+  const fileChooserSetActive = useStoreActions(
+    (a) => a.modals.fileChooser.setActive
+  );
+  const newFileSetActive = useStoreActions((a) => a.modals.newFile.setActive);
+
+  const launchFileChooser = () => fileChooserSetActive(true);
+  const launchNewFile = () => newFileSetActive(true);
 
   const spinnerClasses = classnames({
     visible: backingState.status !== "idle",
@@ -21,8 +28,8 @@ export const MenuBar: React.FC<{}> = () => {
           <RunButton />
         </Navbar.Brand>
         <NavDropdown title="File">
-          <NavDropdown.Item>New</NavDropdown.Item>
-          <NavDropdown.Item>Open</NavDropdown.Item>
+          <NavDropdown.Item onClick={launchNewFile}>New</NavDropdown.Item>
+          <NavDropdown.Item onClick={launchFileChooser}>Open</NavDropdown.Item>
           <NavDropdown.Item>Upload</NavDropdown.Item>
           <NavDropdown.Item>Make a copy</NavDropdown.Item>
           <NavDropdown.Item>Download</NavDropdown.Item>
