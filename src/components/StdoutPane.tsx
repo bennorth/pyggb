857019a80c64eef6e1b4c0e53829d24156eae32f
Ben North <ben@redfrontdoor.org>
2022-12-23 10:00:57 +0000

    Handle overflow of stdout pane

--- src/components/StdoutPane.tsx
+++ src/components/StdoutPane.tsx
@@ -4,8 +4,10 @@ import { useStoreState } from "../store";
 export const StdoutPane: React.FC<{}> = () => {
   const content = useStoreState((s) => s.pyStdout.content);
   return (
-    <div className="stdout">
-      <pre>{content}</pre>
+    <div className="stdout-outer">
+      <div className="stdout-inner abs-0000">
+        <pre>{content}</pre>
+      </div>
     </div>
   );
 };
