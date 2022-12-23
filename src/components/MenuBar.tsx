671125992ab6b913b0ea47be47977fad0de60456
Ben North <ben@redfrontdoor.org>
2022-12-23 10:26:27 +0000

    Add skeleton menubar

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { Container, Navbar, NavDropdown } from "react-bootstrap";
 import { RunButton } from "./RunButton";
 
 export const MenuBar: React.FC<{}> = () => {
   return (
-    <div className="MenuBar">
-      <RunButton />
-    </div>
+    <Navbar>
+      <Container fluid className="MenuBar">
+        <Navbar.Brand>
+          <RunButton />
+        </Navbar.Brand>
+        <NavDropdown title="File">
+          <NavDropdown.Item>New</NavDropdown.Item>
+          <NavDropdown.Item>Open</NavDropdown.Item>
+          <NavDropdown.Item>Upload</NavDropdown.Item>
+          <NavDropdown.Item>Make a copy</NavDropdown.Item>
+          <NavDropdown.Item>Download</NavDropdown.Item>
+        </NavDropdown>
+      </Container>
+    </Navbar>
   );
 };
