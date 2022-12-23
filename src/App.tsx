f908f6822eb6ad95efa1a6c25470dfa83863d7ea
Ben North <ben@redfrontdoor.org>
2022-12-23 15:11:43 +0000

    Render modals

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { IDE } from "./components/IDE";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useStoreActions } from "./store";
+import { FileChooserModal } from "./components/modals/FileChooserModal";
+import { NewFileModal } from "./components/modals/NewFileModal";
 
 function App() {
   const bootDependencies = useStoreActions((a) => a.dependencies.boot);
@@ -12,6 +14,8 @@ function App() {
 
   return (
     <div className="App">
+      <FileChooserModal />
+      <NewFileModal />
       <IDE />
     </div>
   );
