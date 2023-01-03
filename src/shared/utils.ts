0fc8ade2d527c430bab8b0aefef6a27995c6d39f
Ben North <ben@redfrontdoor.org>
2023-01-03 14:24:49 +0000

    fetchAsText(): Add

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -3,3 +3,18 @@
 export function cssValue(property: string): string {
   return getComputedStyle(document.documentElement).getPropertyValue(property);
 }
+
+export const fetchAsText = async (urlWithinApp: string) => {
+  const prefixIsNonEmpty = process.env.PUBLIC_URL !== "";
+  const maybeSeparator = prefixIsNonEmpty ? "/" : "";
+  const url = process.env.PUBLIC_URL + maybeSeparator + urlWithinApp;
+
+  try {
+    const response = await fetch(url);
+    const text = await response.text();
+    return text;
+  } catch (e) {
+    console.error(`fetchAsText("${urlWithinApp}"):`, url, e);
+    return null;
+  }
+};
