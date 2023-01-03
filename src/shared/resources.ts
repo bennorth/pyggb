7b1a154826ab2f9b19c69adf0396e41e14aa2c9c
Ben North <ben@redfrontdoor.org>
2023-01-03 14:24:27 +0000

    ExampleProgramPreview: Define type

--- src/shared/resources.ts
+++ src/shared/resources.ts
@@ -1,3 +1,10 @@
+// TODO: Is this the best place for the following type definition?
+export type ExampleProgramPreview = {
+  path: string;
+  name: string;
+  docMarkdown: string;
+};
+
 export const SkulptGgbModuleUrl = (() => {
   const prefixIsNonEmpty = process.env.PUBLIC_URL !== "";
   const maybeSeparator = prefixIsNonEmpty ? "/" : "";
