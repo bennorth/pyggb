30fe8e0afdc29722cd2e75941e2b1df07487ea5b
Ben North <ben@redfrontdoor.org>
2023-01-10 17:16:36 +0000

    Add, style header for error list

--- src/components/ErrorList.tsx
+++ src/components/ErrorList.tsx
@@ -6,6 +6,9 @@ export const ErrorList: React.FC<{}> = () => {
 
   return (
     <div className="ErrorList abs-0000">
+      <div className="error-list-inner">
+        <h1>Your program has a problem:</h1>
+      </div>
     </div>
   );
 };
