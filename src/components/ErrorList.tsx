128011ef2d04aff0470a31bb56691c632e5382cb
Ben North <ben@redfrontdoor.org>
2023-01-10 17:19:43 +0000

    TracebackEntryItem: Define local component

--- src/components/ErrorList.tsx
+++ src/components/ErrorList.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { TracebackEntry } from "../shared/skulpt-interaction";
 import { useStoreState } from "../store";
+
+type TracebackEntryItemProps = { entry: TracebackEntry };
+const TracebackEntryItem: React.FC<TracebackEntryItemProps> = ({ entry }) => {
+  const rawSource = entry.filename;
+  const source = rawSource === "<stdin>.py" ? "your program" : rawSource;
+
+  // Map linenumber back to the user's original program by taking
+  // account of the "from ggb import *" we inserted at the start when
+  // sending to Skulpt.
+  const userLineNumber = entry.lineno - 2;
+
+  return (
+    <p>
+      line {userLineNumber} (position {entry.colno}) of {source}
+    </p>
+  );
+};
 
 export const ErrorList: React.FC<{}> = () => {
   const errors = useStoreState((s) => s.pyErrors.errors);
