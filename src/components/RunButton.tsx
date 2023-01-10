f7b4b4037e72db66db5da31646b7d64a904eadbf
Ben North <ben@redfrontdoor.org>
2023-01-10 13:40:54 +0000

    Disable RUN button while program running

--- src/components/RunButton.tsx
+++ src/components/RunButton.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { useStoreActions } from "../store";
+import { useStoreActions, useStoreState } from "../store";
 import Button from "react-bootstrap/Button";
 
 export const RunButton: React.FC<{}> = () => {
+  const executionStatus = useStoreState((s) => s.controls.executionStatus);
   const runProgram = useStoreActions((a) => a.controls.runProgram);
+
+  const enabled = executionStatus === "idle";
   return (
     <div className="RunButton" onClick={() => runProgram()}>
-      <Button variant="success" size="sm">
+      <Button variant="success" size="sm" disabled={!enabled}>
         RUN
       </Button>
     </div>
