3785a057c1f8bc9f0470b753ac1625b25165b1d0
Ben North <ben@redfrontdoor.org>
2023-01-10 15:58:09 +0000

    Add error pane below editor
    Includes placeholder text.

--- src/components/IDE.tsx
+++ src/components/IDE.tsx
@@ -9,8 +9,37 @@ export const IDE: React.FC<{}> = () => {
     <div className="pyggb-IDE abs-0000">
       <MenuBar />
       <div className="main-content">
-        <div className="editor">
-          <CodeEditor />
+        <div className="editor-maybe-errors-outer">
+          <div className="editor-maybe-errors-inner abs-0000">
+            <div className="editor">
+              <CodeEditor />
+            </div>
+            {/* TODO: Only show if there are errors. */}
+            <div className="errors-container">
+              <div className="errors abs-0000">
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+                <div>ERRORS GO HERE!</div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="results">
           <div className="ggb">
