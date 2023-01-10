3e14f52278c2e52e1d2ef0121c040de6c049d960
Ben North <ben@redfrontdoor.org>
2023-01-10 17:21:39 +0000

    ErrorList: Include traceback

--- src/components/ErrorList.tsx
+++ src/components/ErrorList.tsx
@@ -51,6 +51,13 @@ export const ErrorList: React.FC<{}> = () => {
     <div className="ErrorList abs-0000">
       <div className="error-list-inner">
         <h1>Your program has a problem:</h1>
+        <ul>
+          {errors.map((e, i) => (
+            <li key={i}>
+              <ErrorReport error={e} />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
