3ed7bfe4c883fb7cb24f4e49fd8050580e83b6bf
Ben North <ben@redfrontdoor.org>
2023-01-10 16:26:41 +0000

    ErrorsList: Crudely display error

--- src/components/ErrorList.tsx
+++ src/components/ErrorList.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { useStoreState } from "../store";
 
 export const ErrorList: React.FC<{}> = () => {
+  const errors = useStoreState((s) => s.pyErrors.errors);
+
   return (
     <div className="ErrorList abs-0000">
+      {errors.length}={errors.toString()}
       <div>ERRORS GO HERE!</div>
       <div>ERRORS GO HERE!</div>
       <div>ERRORS GO HERE!</div>
