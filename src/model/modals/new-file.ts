2223e2ebceae509f67f486c1d39ec11f1f08b06d
Ben North <ben@redfrontdoor.org>
2023-01-17 17:55:49 +0000

    NewFile.initialCodeText: Add
    In model slice for NewFile modal.  Also action setInitialCodeText().

--- src/model/modals/new-file.ts
+++ src/model/modals/new-file.ts
@@ -3,11 +3,17 @@ import { action, Action } from "easy-peasy";
 export type NewFile = {
   active: boolean;
   setActive: Action<NewFile, boolean>;
+  initialCodeText: string | undefined;
+  setInitialCodeText: Action<NewFile, string | undefined>;
 };
 
 export const newFile: NewFile = {
   active: false,
   setActive: action((s, active) => {
     s.active = active;
+  }),
+  initialCodeText: undefined,
+  setInitialCodeText: action((s, initialCodeText) => {
+    s.initialCodeText = initialCodeText;
   }),
 };
