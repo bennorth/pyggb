d3bb704d195d961fa2ebb0616d54266b5c1b3fb8
Ben North <ben@redfrontdoor.org>
2023-01-17 17:56:49 +0000

    NewFile.launch(): Add thunk

--- src/model/modals/new-file.ts
+++ src/model/modals/new-file.ts
@@ -1,10 +1,12 @@
-import { action, Action } from "easy-peasy";
+import { action, Action, thunk, Thunk } from "easy-peasy";
 
 export type NewFile = {
   active: boolean;
   setActive: Action<NewFile, boolean>;
   initialCodeText: string | undefined;
   setInitialCodeText: Action<NewFile, string | undefined>;
+
+  launch: Thunk<NewFile, string | undefined>;
 };
 
 export const newFile: NewFile = {
@@ -15,5 +17,10 @@ export const newFile: NewFile = {
   initialCodeText: undefined,
   setInitialCodeText: action((s, initialCodeText) => {
     s.initialCodeText = initialCodeText;
+  }),
+
+  launch: thunk((a, codeText) => {
+    a.setInitialCodeText(codeText);
+    a.setActive(true);
   }),
 };
