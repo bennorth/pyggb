646716343da4ea74b26a960a2494e776af28897a
Ben North <ben@redfrontdoor.org>
2023-01-26 17:57:57 +0000

    Scroll stdout pane to bottom on new output

--- src/components/StdoutPane.tsx
+++ src/components/StdoutPane.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { createRef, useEffect } from "react";
 import { useStoreState } from "../store";
 
 export const StdoutPane: React.FC<{}> = () => {
   const content = useStoreState((s) => s.pyStdout.content);
+  const divRef = createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    divRef.current!.scrollTop = divRef.current!.scrollHeight;
+  });
+
   return (
     <div className="stdout-outer">
-      <div className="stdout-inner abs-0000">
+      <div ref={divRef} className="stdout-inner abs-0000">
         <pre>{content}</pre>
       </div>
     </div>
