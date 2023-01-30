1aebecb42bba7c187e0a43a4270ee9753e5afe8c
Ben North <ben@redfrontdoor.org>
2023-01-30 16:31:33 +0000

    DownloadPython: Add, include model slice

--- src/model/modals/index.ts
+++ src/model/modals/index.ts
@@ -1,12 +1,15 @@
 import { FileChooser, fileChooser } from "./file-chooser";
 import { NewFile, newFile } from "./new-file";
+import { DownloadPython, downloadPython } from "./download-python";
 
 export type Modals = {
   fileChooser: FileChooser;
   newFile: NewFile;
+  downloadPython: DownloadPython;
 };
 
 export const modals: Modals = {
   fileChooser,
   newFile,
+  downloadPython,
 };
