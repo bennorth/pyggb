47e0cf753995d0184295011c24bcc40acd71840b
Ben North <ben@redfrontdoor.org>
2023-02-10 15:43:17 +0000

    Add "can create new file" test

--- cypress/e2e/file-management.cy.ts
+++ cypress/e2e/file-management.cy.ts
@@ -1,5 +1,32 @@
+import { v4 as uuidv4 } from "uuid";
+
 describe("File management", () => {
   beforeEach(() => {
     cy.visit("/");
+  });
+
+  it("can create new file", () => {
+    const filename = uuidv4();
+
+    cy.get(".MenuBar .nav-link").contains("File").click();
+    cy.get(".dropdown-item").contains("New").click();
+
+    cy.get(".modal-body input").click().type(filename);
+    cy.get("button").contains("Create").click();
+
+    // Name of newly-created file should appear in the navbar / menubar:
+    cy.get(".MenuBar").contains(filename);
+
+    cy.window().then((window) => {
+      const editorText = window["PYGGB_CYPRESS"].ACE_EDITOR.getValue();
+      console.log("editorText", editorText);
+      expect(editorText).to.equal("# Start writing your code!\n");
+
+      window["PYGGB_CYPRESS"].ACE_EDITOR.setValue(
+        `# This is file ${filename}\n`
+      );
+
+      cy.get("#pyggb-ace-editor").click("center");
+    });
   });
 });
