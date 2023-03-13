31b647ad971871df1f5fc19820200159b7accf88
Ben North <ben@redfrontdoor.org>
2023-03-13 15:05:10 +0000

    Add "create two files; switch between" test
    Might well be scope for extracting helper functions.

--- cypress/e2e/file-management.cy.ts
+++ cypress/e2e/file-management.cy.ts
@@ -34,4 +34,53 @@ describe("File management", () => {
       cy.get(".FileChoice").contains(filename);
     });
   });
+
+  it("can create two files and switch between them", () => {
+    const f1 = uuidv4();
+    const f2 = uuidv4();
+
+    chooseFileMenuEntry("New");
+
+    cy.get(".modal-body input").click().type(f1);
+    cy.get("button").contains("Create").click();
+
+    // Name of newly-created file should appear in the navbar / menubar:
+    cy.get(".editor .busy-overlay").should("not.be.visible");
+    cy.get(".MenuBar").contains(f1);
+
+    cy.window().then((window) => {
+      const editorText = window["PYGGB_CYPRESS"].ACE_EDITOR.getValue();
+      expect(editorText).to.equal("# Start writing your code!\n");
+
+      window["PYGGB_CYPRESS"].ACE_EDITOR.setValue(`# This is file ${f1}\n`);
+      chooseFileMenuEntry("Save now");
+
+      chooseFileMenuEntry("New");
+      cy.get(".modal-body input").click().type(f2);
+      cy.get("button").contains("Create").click();
+
+      cy.get(".editor .busy-overlay").should("not.be.visible");
+      cy.get(".MenuBar").contains(f2);
+
+      cy.window().then((window) => {
+        const editorText = window["PYGGB_CYPRESS"].ACE_EDITOR.getValue();
+        console.log("editorText", editorText);
+        expect(editorText).to.equal("# Start writing your code!\n");
+
+        window["PYGGB_CYPRESS"].ACE_EDITOR.setValue(`# This is file ${f2}\n`);
+        chooseFileMenuEntry("Save now");
+        chooseFileMenuEntry("Open");
+
+        cy.get(".FileChoice").contains(f1).click();
+        cy.get(".editor .busy-overlay").should("not.be.visible");
+        cy.get(".MenuBar").contains(f1);
+
+        cy.window().then((window) => {
+          const editorText = window["PYGGB_CYPRESS"].ACE_EDITOR.getValue();
+          console.log("editorText", editorText);
+          expect(editorText).to.equal(`# This is file ${f1}\n`);
+        });
+      });
+    });
+  });
 });
