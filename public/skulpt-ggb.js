c31f78afa80a5d45bd9afb948ba74aba3d7ff3a3
Ben North <ben@redfrontdoor.org>
2023-03-13 13:11:37 +0000

    sharedGetSets: Add
    For properties
    
        is_visible
        is_independent
        color
        size
        line_thickness

--- public/skulpt-ggb.js
+++ public/skulpt-ggb.js
@@ -74,6 +74,53 @@ function $builtinmodule() {
     return gotType === requiredType;
   };
 
+  const sharedGetSets = {
+    is_visible: {
+      $get() {
+        return new Sk.builtin.bool(ggbApi.getVisible(this.$ggbLabel));
+      },
+      $set(pyIsVisible) {
+        const isVisible = Sk.misceval.isTrue(pyIsVisible);
+        ggbApi.setVisible(this.$ggbLabel, isVisible);
+      },
+    },
+    is_independent: {
+      $get() {
+        return new Sk.builtin.bool(ggbApi.isIndependent(this.$ggbLabel));
+      },
+    },
+    color: {
+      $get() {
+        const color = ggbApi.getColor(this.$ggbLabel);
+        return new Sk.builtin.str(color);
+      },
+      $set(pyColor) {
+        if (!Sk.builtin.checkString(pyColor))
+          throw new Sk.builtin.TypeError("color must be string");
+        const mRGB = parseColorOrFail(pyColor.v);
+        ggbApi.setColor(this.$ggbLabel, ...mRGB);
+      },
+    },
+    size: {
+      $get() {
+        return new Sk.builtin.float_(ggbApi.getPointSize(this.$ggbLabel));
+      },
+      $set(pySize) {
+        // TODO: Verify integer and in range [1, 9]
+        ggbApi.setPointSize(this.$ggbLabel, pySize.v);
+      },
+    },
+    line_thickness: {
+      $get() {
+        return new Sk.builtin.int_(ggbApi.getLineThickness(this.$ggbLabel));
+      },
+      $set(pyThickness) {
+        // TODO: Verify integer and in range [1, 9]
+        ggbApi.setLineThickness(this.$ggbLabel, pyThickness.v);
+      },
+    },
+  };
+
   mod.Point = Sk.abstr.buildNativeClass("Point", {
     constructor: function Point(spec) {
       switch (spec.kind) {
