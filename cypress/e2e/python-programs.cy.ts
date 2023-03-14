e6f5160a8e449eeb537cc3f72d9d1801edcc5672
Ben North <ben@redfrontdoor.org>
2023-03-14 22:40:19 +0000

    Add test specs

--- cypress/e2e/python-programs.cy.ts
+++ cypress/e2e/python-programs.cy.ts
@@ -61,7 +61,241 @@ describe("Runs Python programs", { testIsolation: false }, () => {
   };
 
   const runsWithoutErrorSpecs: Array<RunsWithoutErrorSpec> = [
-    // TODO: specs
+    {
+      label: "Line(Point, Point)",
+      code: `
+        A = Point(3, 4)
+        B = Point(2, 1)
+        k = Line(A, B)
+      `,
+    },
+    {
+      label: "Point(x, y, kwargs)",
+      code: `
+        A = Point(3, 4, color="#f00", size=8)
+        B = Point(1, -5, is_visible=False)
+        print(A.color)
+      `,
+      expOutputs: ["#FF0000"],
+    },
+    {
+      label: "Number",
+      code: `
+        x = Number(3.25)
+        print("x1 =", x.value)
+        x.value = 4.5
+        print("x2 =", x.value)
+      `,
+      expOutputs: ["x1 = 3.25", "x2 = 4.5"],
+    },
+    {
+      label: "Boolean",
+      code: `
+        x = Boolean(True)
+        print("x1 =", x.value)
+        x.value = False
+        print("x2 =", x.value)
+      `,
+      expOutputs: ["x1 = True", "x2 = False"],
+    },
+    {
+      label: "Circle(Point, radius)",
+      code: `
+        A = Point(3, 4)
+        k = Circle(A, 2)
+        l = Circle(A, Number(2))
+      `,
+    },
+    {
+      label: "Circle(Point, Point)",
+      code: `
+        A = Point(3, 2)
+        B = Point(6, 2)
+        k = Circle(A, B)
+        print("radius =", k.radius)
+      `,
+      expOutputs: ["radius = 3.0"],
+    },
+    {
+      label: "Circle(Point, Point, Point)",
+      code: `
+        A = Point(2, 0)
+        B = Point(0, 2)
+        C = Point(-2, 0)
+        k = Circle(A, B, C)
+        print("radius =", k.radius)
+      `,
+      expOutputs: ["radius = 2.0"],
+    },
+    {
+      label: "Vector(Point, Point)",
+      code: `
+        A = Point(3, 0)
+        B = Point(0, 4)
+        v1 = Vector(A, B)
+      `,
+    },
+    {
+      label: "Vector(Number, Number)",
+      code: `
+        v1 = Vector(3, 4)
+        v2 = Vector(2, Number(1))
+        v3 = Vector(Number(4), -1)
+        v4 = Vector(Number(3), Number(-2))
+      `,
+    },
+    {
+      label: "Segment(Point, Point)",
+      code: `
+        A = Point(3, 0)
+        B = Point(0, 4)
+        k = Segment(A, B, line_thickness=8)
+        print("length =", k.length)
+        print("thickness =", k.line_thickness)
+      `,
+      expOutputs: ["length = 5.0", "thickness = 8"],
+    },
+    {
+      label: "Polygon(Array<Points>)",
+      code: `
+        A = Point(3, 0)
+        B = Point(0, 4)
+        C = Point(-2, 1)
+        D = Point(-1, -3)
+        p = Polygon([A, B, C, D])
+        print("area =", p.area)
+      `,
+      expOutputs: ["area = 18.0"], // Pick's theorem
+    },
+    {
+      label: "Polygon(Point, Point, Integer)",
+      code: `
+        A = Point(-2, -2)
+        B = Point(0, -2)
+        p = Polygon(A, B, 6)
+      `,
+    },
+    {
+      label: "Parabola(Point, Line)",
+      code: `
+        A = Point(0, Number(-4))
+        k = Line(Point(-1, -5), Point(1, -5))
+        p = Parabola(A, k)
+      `,
+    },
+    {
+      label: "Distance(Point, Point)",
+      code: `
+        A = Point(-3, 0)
+        B = Point(0, -4)
+        print("AB =", Distance(A, B))
+      `,
+      expOutputs: ["AB = 5.0"],
+    },
+    {
+      label: "Distance(Point, Line)",
+      code: `
+        A = Point(2, -2)
+        B = Point(-4, 0)
+        C = Point(0, 4)
+        k = Line(B, C)
+        d = Distance(A, k)
+        print(f"Ak = {d:.4f}")
+      `,
+      expOutputs: ["Ak = 5.6569"], // 4√2
+    },
+    {
+      label: "Distance(Point, Polygon)",
+      code: `
+        A = Point(2, -2)
+        B = Point(-1, 2)
+        C = Point(-0.5, 3)
+        p = Polygon(B, C, 6)
+        d = Distance(A, p)
+        print("Ap =", d)
+      `,
+      expOutputs: ["Ap = 5.0"], // Nearest point is vertex B
+    },
+    {
+      label: "Intersect(Line, Line)",
+      code: `
+        A = Point(0, 0)
+        B = Point(2, 4)
+        k1 = Line(A, B)
+        C = Point(2, 1)
+        D = Point(0, 3)
+        k2 = Line(C, D)
+        Es = Intersect(k1, k2)
+        print(len(Es), "intersection/s")
+        print(f"Es[0] = ({Es[0].x}, {Es[0].y})")
+      `,
+      expOutputs: ["1 intersection/s", "Es[0] = (1.0, 2.0)"],
+    },
+    {
+      label: "Intersect(Circle, Circle)",
+      code: `
+        from operator import attrgetter
+        k1 = Circle(Point(3, 0), 5)
+        k2 = Circle(Point(-3, 0), 5)
+        Es = sorted(Intersect(k1, k2), key=attrgetter("y"))
+        print(len(Es), "intersection/s")
+        print(f"Es[0] = ({Es[0].x}, {Es[0].y})")
+        print(f"Es[1] = ({Es[1].x}, {Es[1].y})")
+      `,
+      expOutputs: [
+        "2 intersection/s",
+        "Es[0] = (0.0, -4.0)",
+        "Es[1] = (0.0, 4.0)",
+      ],
+    },
+    {
+      label: "Rotate(Vector)",
+      code: `
+        import math
+        A = Point(1, 3)
+        v = Vector(2, 0)
+        B = A + Rotate(v, math.pi / 4.0)
+        print(f"B = ({B.x:.4f}, {B.y:.4f})")
+      `,
+      expOutputs: ["B = (2.4142, 4.4142)"],
+    },
+    {
+      label: "ClearConsole",
+      code: `
+        print("hello world")
+        ClearConsole()
+        print("that's all folks")
+      `,
+      expOutputs: ["that's all folks"],
+      expNonOutputs: ["hello world"],
+    },
+    {
+      label: "Function.sin/cos",
+      code: `
+        import math
+        sin_pi_3 = Function.sin(Number(math.pi / 3.0)).value
+        cos_pi_3 = Function.cos(Number(math.pi / 3.0)).value
+        print(f"sin = {sin_pi_3:.4f}")
+        print(f"cos = {cos_pi_3:.4f}")
+      `,
+      expOutputs: ["sin = 0.8660", "cos = 0.5000"],
+    },
+    {
+      label: "Number ops",
+      code: `
+        x = Number(2.0)
+        y = 4.0
+        print((x + y).value == 6.0)
+        print((y + x).value == 6.0)
+        print((x - y).value == -2.0)
+        print((y - x).value == 2.0)
+        print((x * y).value == 8.0)
+        print((y * x).value == 8.0)
+        print((y / x).value == 2.0)
+        print((x / y).value == 0.5)
+      `,
+      expNonOutputs: ["False"],
+    },
   ];
 
   runsWithoutErrorSpecs.forEach((spec) => {
