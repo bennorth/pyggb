c2e2d5b237ab01dffd72931069cc71e36b7ced1d
Ben North <ben@redfrontdoor.org>
2023-04-02 09:12:47 +0100

    Remove unused code from skulpt-ggb.js

--- public/skulpt-ggb.js
+++ public/skulpt-ggb.js
@@ -8,35 +8,6 @@ function $builtinmodule() {
   return globalThis.$skulptGgbModule(appApi);
   /*
   const ggbApi = appApi.ggb;
-  const skApi = appApi.sk;
-  const uiApi = appApi.ui;
-
   PYGGB_CYPRESS().GGB_API = ggbApi;
-
-  let mod = {};
-
-  const namesForExport = Sk.ffi.remapToPy([
-    "Number",
-    "Boolean",
-    "Point",
-    "Circle",
-    "Line",
-    "Vector",
-    "Segment",
-    "Polygon",
-    "Parabola",
-    // "Slider",  // This is not ready for use yet.
-    "Distance",
-    "Intersect",
-    "Rotate",
-    "Function",
-    "If",
-    "ClearConsole",
-  ]);
-
-  mod.__name__ = new Sk.builtin.str("ggb");
-  mod.__all__ = namesForExport;
-
-  return mod;
 */
 }
