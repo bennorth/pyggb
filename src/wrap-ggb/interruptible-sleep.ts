265209b10c9a6fd29d541d26caecfe308318c525
Ben North <ben@redfrontdoor.org>
2023-04-07 13:09:00 +0100

    interruptible-sleep: Add skeleton

---
+++ src/wrap-ggb/interruptible-sleep.ts
@@ -0,0 +1,16 @@
+import { throwIfNotNumber } from "./shared";
+import { SkObject, SkulptApi } from "./skulptapi";
+
+declare var Sk: SkulptApi;
+
+export function interruptibleSleep(
+  pyDelayS: SkObject
+) {
+  throwIfNotNumber(pyDelayS, "delay");
+  const delayMs = 1000 * pyDelayS.v;
+  // TODO
+}
+
+export function register(mod: any, appApi: AppApi) {
+  // TODO
+}
