dd74f5663b924f2884b69f123c479e564f20e264
Ben North <ben@redfrontdoor.org>
2023-04-11 12:49:31 +0100

    Update "intersect line and parabola" example
    Now works.

--- public/examples/intersect_line_parabola.py
+++ public/examples/intersect_line_parabola.py
@@ -1,20 +1,16 @@
 """
 Intersect a line and a parabola
 
-**Does not work yet**
-
 Create a parabola which can be adjusted using a slider, and find its
 intersection with a fixed line.
 """
 
-num = Slider(1, 5, increment=0.1)
-parabola = evalCommand("y=" + num + " x^2")
-line = evalCommand("y = x")
+coeff_b = Slider(1, 5, increment=0.1)
+parabola = Parabola(1.5, coeff_b, 0)
+line = Line(1, 0)
 
 print(parabola)
 print(line)
 
-intersect(parabola, line)
-
-## TODO: Would "num.value = 2.0" be more Pythonic?
-setValue(num, 2)
+coeff_b.value = 4.0
+print(Intersect(parabola, line))
