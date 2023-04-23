136e152e24da0b17fd19b503888de67955493cba
Ben North <ben@redfrontdoor.org>
2023-04-23 19:56:50 +0100

    FileChoiceActivity: Define type
    Includes disjunct for to-be-implemented deletion of user files.

--- src/model/modals/file-chooser.ts
+++ src/model/modals/file-chooser.ts
@@ -1,5 +1,12 @@
 import { Action } from "easy-peasy";
 import { propSetterAction } from "../../shared/utils";
+import { UserFilePreview } from "../../shared/db";
+
+export type FileChoiceActivity =
+  | { kind: "none" }
+  | { kind: "choose-user-file" }
+  | { kind: "choose-example" }
+  | ({ kind: "confirm-delete-user-file" } & UserFilePreview);
 
 export type FileChooser = {
   active: boolean;
