2f109f0ba94da30987bd78aef9b829aa0ae1f393
Ben North <ben@redfrontdoor.org>
2023-04-24 12:53:14 +0100

    Simplify "bad constructor" tests

--- cypress/e2e/python-programs.cy.ts
+++ cypress/e2e/python-programs.cy.ts
@@ -588,16 +588,9 @@ describe("Handles bad constructor calls", optsNoIsolation, () => {
     },
   ];
 
-  badConstructionSpecs.forEach((spec) => {
-    it(`handles ${spec.label} ok`, () => {
-      cy.window().then((window) => {
-        const code = deIndent(spec.code);
-        window["PYGGB_CYPRESS"].ACE_EDITOR.setValue(code);
-        cy.get("button").contains("RUN").click();
-        spec.assertions.forEach((assertion) => assertion());
-      });
-    });
-  });
+  badConstructionSpecs.forEach((spec) =>
+    it(`handles ${spec.label} ok`, runBadCode(spec))
+  );
 });
 
 /**
