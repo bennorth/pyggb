78bdb5e7f4d87cc1b3006bed63e0aa8907d14a38
Ben North <ben@redfrontdoor.org>
2023-04-29 14:54:08 +0100

    Add AboutPyGgb slice to model

--- src/model/modals/index.ts
+++ src/model/modals/index.ts
@@ -1,15 +1,18 @@
 import { FileChooser, fileChooser } from "./file-chooser";
 import { NewFile, newFile } from "./new-file";
 import { DownloadPython, downloadPython } from "./download-python";
+import { AboutPyGgb, aboutPyGgb } from "./about-pyggb";
 
 export type Modals = {
   fileChooser: FileChooser;
   newFile: NewFile;
   downloadPython: DownloadPython;
+  aboutPyGgb: AboutPyGgb;
 };
 
 export const modals: Modals = {
   fileChooser,
   newFile,
   downloadPython,
+  aboutPyGgb,
 };
