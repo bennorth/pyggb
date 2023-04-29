7c8e1a31da96b042e70b511be19909ce90ecd3e0
Ben North <ben@redfrontdoor.org>
2023-04-29 14:54:46 +0100

    AboutPyGgbModal: Add outline; show if active

--- src/components/modals/AboutPyGgbModal.tsx
+++ src/components/modals/AboutPyGgbModal.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { Button, Modal } from "react-bootstrap";
 import { useStoreActions, useStoreState } from "../../store";
 
 export const AboutPyGgbModal: React.FC<{}> = () => {
+  const isActive = useStoreState((s) => s.modals.aboutPyGgb.active);
+  const setActive = useStoreActions((a) => a.modals.aboutPyGgb.setActive);
+  const dismiss = () => setActive(false);
+
+  return (
+    <Modal className="AboutPyGgbModal" show={isActive} animation={false}>
+      <Modal.Header>
+        <Modal.Title>About PyGgb</Modal.Title>
+      </Modal.Header>
+      <Modal.Footer>
+        <Button onClick={dismiss}>OK</Button>
+      </Modal.Footer>
+    </Modal>
+  );
 };
