b02762910b5b250e1f2ec00e1b00d4dae8b0a011
Ben North <ben@redfrontdoor.org>
2023-05-06 13:37:43 +0100

    Parabola ctor: Simplify via setGgbLabel helpers

--- src/wrap-ggb/wrappings/parabola.ts
+++ src/wrap-ggb/wrappings/parabola.ts
@@ -5,6 +5,8 @@ import {
   SkGgbObject,
   AugmentedGgbApi,
   withPropertiesFromNameValuePairs,
+  setGgbLabelFromArgs,
+  setGgbLabelFromCmd,
 } from "../shared";
 import { SkObject, SkulptApi } from "../../shared/vendor-types/skulptapi";
 import { registerObjectType } from "../type-registry";
@@ -36,26 +38,23 @@ export const register = (mod: any, appApi: AppApi) => {
       this: SkGgbParabola,
       spec: SkGgbParabolaCtorSpec
     ) {
+      const setLabelArgs = setGgbLabelFromArgs(ggb, this, "Parabola");
+      const setLabelCmd = setGgbLabelFromCmd(ggb, this);
+
       switch (spec.kind) {
         case "wrap-existing": {
           this.$ggbLabel = spec.label;
           break;
         }
         case "focus-directrix": {
-          const ggbArgs = `${spec.focus.$ggbLabel},${spec.directrix.$ggbLabel}`;
-          const ggbCmd = `Parabola(${ggbArgs})`;
-          const lbl = ggb.evalCmd(ggbCmd);
-          this.$ggbLabel = lbl;
+          setLabelArgs([spec.focus.$ggbLabel, spec.directrix.$ggbLabel]);
           this.focus = spec.focus;
           this.directrix = spec.directrix;
-          console.log("Made Parabola?", lbl, spec);
           break;
         }
         case "coefficients": {
-          const ggbCoeffs = spec.coeffs.map(ggb.numberValueOrLabel);
-          const ggbCmd = `y=(${ggbCoeffs[0]})x^2 + (${ggbCoeffs[1]})x + (${ggbCoeffs[2]})`;
-          const lbl = ggb.evalCmd(ggbCmd);
-          this.$ggbLabel = lbl;
+          const [a, b, c] = spec.coeffs.map(ggb.numberValueOrLabel);
+          setLabelCmd(`y=(${a})x^2 + (${b})x + (${c})`);
           // TODO: Set focus and directrix?
           break;
         }
