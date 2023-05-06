e56f82f0aca93d40810ac051da82f8ab0d51a197
Ben North <ben@redfrontdoor.org>
2023-05-06 11:54:16 +0100

    Ellipse.tp$new(): Define

--- src/wrap-ggb/wrappings/ellipse.ts
+++ src/wrap-ggb/wrappings/ellipse.ts
@@ -81,6 +81,55 @@ export const register = (mod: any, appApi: AppApi) => {
           );
       }
     },
+    slots: {
+      tp$new(args, kwargs) {
+        const badArgsError = new Sk.builtin.TypeError(
+          "Ellipse() arguments must be" +
+            " (focus, focus, semimajor_axis_length)," +
+            " (focus, focus, semimajor_axis_segment)," +
+            " or (focus, focus, point)"
+        );
+
+        const make = (spec: SkGgbEllipseCtorSpec) =>
+          withPropertiesFromNameValuePairs(new mod.Ellipse(spec), kwargs);
+
+        switch (args.length) {
+          case 3:
+            if (
+              ggb.isGgbObjectOfType(args[0], "point") &&
+              ggb.isGgbObjectOfType(args[1], "point")
+            ) {
+              if (ggb.isPythonOrGgbNumber(args[2])) {
+                return make({
+                  kind: "foci-semimajor-axis-length",
+                  focus1: args[0],
+                  focus2: args[1],
+                  semimajorAxis: args[2],
+                });
+              }
+              if (ggb.isGgbObjectOfType(args[2], "segment")) {
+                return make({
+                  kind: "foci-semimajor-axis-segment",
+                  focus1: args[0],
+                  focus2: args[1],
+                  semimajorAxis: args[2],
+                });
+              }
+              if (ggb.isGgbObjectOfType(args[2], "point")) {
+                return make({
+                  kind: "foci-point",
+                  focus1: args[0],
+                  focus2: args[1],
+                  point: args[2],
+                });
+              }
+            }
+            throw badArgsError;
+          default:
+            throw badArgsError;
+        }
+      },
+    },
   });
 
   mod.Ellipse = cls;
