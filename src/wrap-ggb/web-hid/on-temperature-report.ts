4cfa6fabfec261bf8c690f2d32bb1e6acbf1f03e
Ben North <ben@redfrontdoor.org>
2023-05-06 08:36:01 +0100

    valueOfEvent(): Define

--- src/wrap-ggb/web-hid/on-temperature-report.ts
+++ src/wrap-ggb/web-hid/on-temperature-report.ts
@@ -8,3 +8,20 @@ function deviceId(device: HIDDevice): string {
   const productId = device.productId.toString(16).padStart(4, "0");
   return `${vendorId}:${productId}`;
 }
+
+function valueOfEvent(event: HIDInputReportEvent): number {
+  const device = event.target as HIDDevice;
+  switch (deviceId(device)) {
+    case "046d:c214":
+      // Logitech, Inc. ATK3 (Attack III Joystick)
+      return event.data.getUint8(2) / 255.0;
+    case "08f7:0002":
+      // Vernier Go!Temp
+      const rawTemp = event.data.getInt16(2, true);
+      const rawCelsius = 0.0078117735 * rawTemp + 0.4979364513;
+      const celsius = Math.round(rawCelsius * 10) / 10;
+      return celsius;
+    default:
+      return 0;
+  }
+}
