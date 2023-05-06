bb7cc8521e0469961ca332928c3868cb11b5044b
Ben North <ben@redfrontdoor.org>
2023-05-06 11:54:04 +0100

    Ellipse: Define ctor

--- src/wrap-ggb/wrappings/ellipse.ts
+++ src/wrap-ggb/wrappings/ellipse.ts
@@ -39,7 +39,48 @@ export const register = (mod: any, appApi: AppApi) => {
   const ggb = augmentedGgbApi(appApi.ggb);
 
   const cls = Sk.abstr.buildNativeClass("Ellipse", {
-    // TODO
+    constructor: function Ellipse(
+      this: SkGgbEllipse,
+      spec: SkGgbEllipseCtorSpec
+    ) {
+      switch (spec.kind) {
+        case "wrap-existing": {
+          this.$ggbLabel = spec.label;
+          break;
+        }
+        case "foci-semimajor-axis-length": {
+          const focus1 = spec.focus1.$ggbLabel;
+          const focus2 = spec.focus2.$ggbLabel;
+          const smAxis = ggb.numberValueOrLabel(spec.semimajorAxis);
+          const ggbCmd = `Ellipse(${focus1},${focus2},${smAxis})`;
+          const ggbLbl = ggb.evalCmd(ggbCmd);
+          this.$ggbLabel = ggbLbl;
+          break;
+        }
+        case "foci-semimajor-axis-segment": {
+          const focus1 = spec.focus1.$ggbLabel;
+          const focus2 = spec.focus2.$ggbLabel;
+          const smAxis = spec.semimajorAxis.$ggbLabel;
+          const ggbCmd = `Ellipse(${focus1},${focus2},${smAxis})`;
+          const ggbLbl = ggb.evalCmd(ggbCmd);
+          this.$ggbLabel = ggbLbl;
+          break;
+        }
+        case "foci-point": {
+          const focus1 = spec.focus1.$ggbLabel;
+          const focus2 = spec.focus2.$ggbLabel;
+          const point = spec.point.$ggbLabel;
+          const ggbCmd = `Ellipse(${focus1},${focus2},${point})`;
+          const ggbLbl = ggb.evalCmd(ggbCmd);
+          this.$ggbLabel = ggbLbl;
+          break;
+        }
+        default:
+          throw new Sk.builtin.RuntimeError(
+            `bad Ellipse spec kind "${(spec as any).kind}"`
+          );
+      }
+    },
   });
 
   mod.Ellipse = cls;
