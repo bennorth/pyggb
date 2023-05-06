cfc5c4df287a29138ba13736863b8e27950c2dc6
Ben North <ben@redfrontdoor.org>
2023-05-06 08:35:11 +0100

    deviceId(): Define helper

--- src/wrap-ggb/web-hid/on-temperature-report.ts
+++ src/wrap-ggb/web-hid/on-temperature-report.ts
@@ -2,3 +2,9 @@ import { AppApi } from "../../shared/appApi";
 import { SkObject, SkulptApi } from "../../shared/vendor-types/skulptapi";
 
 declare var Sk: SkulptApi;
+
+function deviceId(device: HIDDevice): string {
+  const vendorId = device.vendorId.toString(16).padStart(4, "0");
+  const productId = device.productId.toString(16).padStart(4, "0");
+  return `${vendorId}:${productId}`;
+}
