81ff4af6420a936f44b9aec5f32623c49b74a0a9
Ben North <ben@redfrontdoor.org>
2023-05-06 12:48:46 +0100

    Update comments

--- src/wrap-ggb/wrappings/intersect.ts
+++ src/wrap-ggb/wrappings/intersect.ts
@@ -8,15 +8,14 @@ import { SkulptApi } from "../../shared/vendor-types/skulptapi";
 
 declare var Sk: SkulptApi;
 
-// TODO: Is this the best way to handle intersections?  Could instead
-// run command
-//
-//     {Intersect(⋯)}
-//
-// (i.e., wrapped in {}s), which gives us a dynamic GGB list.  Would
-// then really have to wrap normal Python sequence operations to this
-// object, which could only be done imperfectly.  E.g., Python sorted(⋯)
-// always returns a native Python list.
+// The right way to present Intersect() to Python is not obvious.  The
+// native Ggb return value is a list/array, which we could wrap in a
+// sequence-like object to Python.  This would have the advantage of
+// letting the list of intersection points update as the intersecting
+// objects move.  However, some Python operations on sequences e.g.,
+// sorted(), return actual Python lists so at that point we'd lose the
+// tie to the native Ggb array of intersections.  For v1, we settled on
+// only supporting the Intersect(p, q, n) form of the Ggb function.
 
 export const register = (mod: any, appApi: AppApi) => {
   const ggb = augmentedGgbApi(appApi.ggb);
@@ -58,17 +57,6 @@ export const register = (mod: any, appApi: AppApi) => {
     //
     // If you intersect two Segments which are collinear and overlap, you get
     // back a NaN,Nan point.
-    //
-    // Not clear what a good user experience here is.  Doesn't fit so well with
-    // Python's idea of doing a calculation and getting a result.
-
-    // Not sure why we get a label "null" when intersecting two circles,
-    // but exclude such labels.
-    const points = labels
-      .filter((lbl) => lbl !== "null")
-      .map((label) => new mod.Point({ kind: "wrap-existing", label }));
-
-    return new Sk.builtin.list(points);
   });
 
   mod.Intersect = fun;
