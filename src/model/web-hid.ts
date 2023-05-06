28b08a00b3087c0ca3f69642fb32fce68a03b2f1
Ben North <ben@redfrontdoor.org>
2023-05-06 08:16:01 +0100

    WebHid.acquisitionStatus: Add
    And type and setter action.

--- src/model/web-hid.ts
+++ src/model/web-hid.ts
@@ -1,8 +1,18 @@
 import { Action, Thunk, action, thunk } from "easy-peasy";
 import { propSetterAction } from "../shared/utils";
 
+type DeviceAcquisitionStatus =
+  | "not-yet-requested"
+  | "requesting"
+  | "failed"
+  | "succeeded";
+
 export type WebHid = {
+  acquisitionStatus: DeviceAcquisitionStatus;
+  setAcquisitionStatus: Action<WebHid, DeviceAcquisitionStatus>;
 };
 
 export let webHid: WebHid = {
+  acquisitionStatus: "not-yet-requested",
+  setAcquisitionStatus: propSetterAction("acquisitionStatus"),
 };
