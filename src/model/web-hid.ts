19c03f1cee0ecb5f0b72b280e5cfe0e393728a75
Ben North <ben@redfrontdoor.org>
2023-05-06 08:16:49 +0100

    Define USB filters for initial test devices

--- src/model/web-hid.ts
+++ src/model/web-hid.ts
@@ -1,6 +1,19 @@
 import { Action, Thunk, action, thunk } from "easy-peasy";
 import { propSetterAction } from "../shared/utils";
 
+const kDeviceFilters = [
+  {
+    // Logitech Attack III Joystick
+    vendorId: 0x046d,
+    productId: 0xc214,
+  },
+  {
+    // Vernier Go!Temp
+    vendorId: 0x08f7,
+    productId: 0x0002,
+  },
+];
+
 type DeviceAcquisitionStatus =
   | "not-yet-requested"
   | "requesting"
