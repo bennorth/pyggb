7094e29390124109b7cea5c8a179de0c15c19419
Ben North <ben@redfrontdoor.org>
2023-05-10 10:03:02 +0100

    Wrap fs.readdir() as Cypress task

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "cypress";
+import * as fs from "node:fs/promises";
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000/",
     viewportWidth: 1200,
     viewportHeight: 1024,
+
+    setupNodeEvents(on, _config) {
+      on("task", {
+        readdir: fs.readdir,
+      });
+    },
   },
 });
