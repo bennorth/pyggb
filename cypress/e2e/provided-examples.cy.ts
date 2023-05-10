b176edcb5bd184b286872c87cc6fa4c4324736dc
Ben North <ben@redfrontdoor.org>
2023-05-10 09:33:24 +0100

    Test each spec

--- cypress/e2e/provided-examples.cy.ts
+++ cypress/e2e/provided-examples.cy.ts
@@ -13,4 +13,27 @@ describe("runs provided examples", optsNoIsolation, () => {
   const specs: Array<SampleCodeSpec> = [
     // TODO
   ];
+
+  specs.forEach((spec) => {
+    it(`runs example "${spec.filename}"`, () => {
+      if (spec.skip ?? false) {
+        return;
+      }
+
+      cy.fixture(`examples/${spec.filename}`, "utf-8").then((code) => {
+        cy.window().then((window) => {
+          window["PYGGB_CYPRESS"]["ZERO_DELAY"] = true;
+          const fullCode = code + '\n\nprint("done")';
+          window["PYGGB_CYPRESS"].ACE_EDITOR.setValue(fullCode);
+          cy.get("button").contains("RUN").click();
+          if (spec.expDoneOutput ?? true) {
+            cy.get(".stdout-inner").contains("done");
+          }
+          (spec.expOutputs ?? []).forEach((expOutput) =>
+            cy.get(".stdout-inner").contains(`${expOutput}\n`)
+          );
+        });
+      });
+    });
+  });
 });
