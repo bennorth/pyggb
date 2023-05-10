39f23de95b1f7f78d1b5b8196c04fded8798323f
Ben North <ben@redfrontdoor.org>
2023-05-10 09:32:45 +0100

    SampleCodeSpec: Define type and skeleton array

--- cypress/e2e/provided-examples.cy.ts
+++ cypress/e2e/provided-examples.cy.ts
@@ -2,4 +2,15 @@ import { createNewPyGgbFile, optsNoIsolation } from "./shared";
 
 describe("runs provided examples", optsNoIsolation, () => {
   before(createNewPyGgbFile);
+
+  type SampleCodeSpec = {
+    filename: string;
+    skip?: boolean;
+    expDoneOutput?: boolean;
+    expOutputs?: Array<string>;
+  };
+
+  const specs: Array<SampleCodeSpec> = [
+    // TODO
+  ];
 });
