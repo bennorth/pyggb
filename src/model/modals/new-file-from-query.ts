24b1b8b5099fd4e8cd9b59d076bef37e852388e0
Ben North <ben@redfrontdoor.org>
2023-05-26 10:54:51 +0100

    NewFileFromQuery: Define skeleton model slice

--- src/model/modals/new-file-from-query.ts
+++ src/model/modals/new-file-from-query.ts
@@ -1,3 +1,5 @@
+import { Action, Thunk, thunk } from "easy-peasy";
+import { propSetterAction } from "../../shared/utils";
 import { AsyncInflateOptions, decompress } from "fflate";
 
 function zlibDecompress(
@@ -21,3 +23,13 @@ type NewFileFromQueryState =
   | { kind: "offering"; name: string; codeText: string };
 
 const idleState: NewFileFromQueryState = { kind: "idle" };
+
+export type NewFileFromQuery = {
+  state: NewFileFromQueryState;
+  setState: Action<NewFileFromQuery, NewFileFromQueryState>;
+};
+
+export let newFileFromQuery: NewFileFromQuery = {
+  state: idleState,
+  setState: propSetterAction("state"),
+};
