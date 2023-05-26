0c2dd1dcb232f0e8941fad9101f133e15bebcb1d
Ben North <ben@redfrontdoor.org>
2023-05-26 10:51:40 +0100

    ShareAsUrl: Include slice in app model

--- src/model/modals/index.ts
+++ src/model/modals/index.ts
@@ -2,12 +2,14 @@ import { FileChooser, fileChooser } from "./file-chooser";
 import { NewFile, newFile } from "./new-file";
 import { DownloadPython, downloadPython } from "./download-python";
 import { AboutPyGgb, aboutPyGgb } from "./about-pyggb";
+import { ShareAsUrl, shareAsUrl } from "./share-as-url";
 
 export type Modals = {
   fileChooser: FileChooser;
   newFile: NewFile;
   downloadPython: DownloadPython;
   aboutPyGgb: AboutPyGgb;
+  shareAsUrl: ShareAsUrl;
 };
 
 export const modals: Modals = {
@@ -15,4 +17,5 @@ export const modals: Modals = {
   newFile,
   downloadPython,
   aboutPyGgb,
+  shareAsUrl,
 };
