c36812e2d4309efce21441d3fcfd9836197f2133
Ben North <ben@redfrontdoor.org>
2023-05-26 10:56:21 +0100

    NewFileFromQuery.acceptOffer(): Add thunk

--- src/model/modals/new-file-from-query.ts
+++ src/model/modals/new-file-from-query.ts
@@ -1,5 +1,6 @@
 import { Action, Thunk, thunk } from "easy-peasy";
 import { propSetterAction } from "../../shared/utils";
+import { PyGgbModel } from "..";
 import { AsyncInflateOptions, decompress } from "fflate";
 
 function zlibDecompress(
@@ -28,12 +29,24 @@ export type NewFileFromQuery = {
   state: NewFileFromQueryState;
   setState: Action<NewFileFromQuery, NewFileFromQueryState>;
 
+  acceptOffer: Thunk<NewFileFromQuery, void, any, PyGgbModel>;
   rejectOffer: Thunk<NewFileFromQuery>;
 };
 
 export let newFileFromQuery: NewFileFromQuery = {
   state: idleState,
   setState: propSetterAction("state"),
+
+  acceptOffer: thunk(async (a, _voidPayload, helpers) => {
+    const state = helpers.getState().state;
+    if (state.kind !== "offering") {
+      throw new Error("acceptOffer(): not offering");
+    }
+
+    const descriptor = { name: state.name, codeText: state.codeText };
+    await helpers.getStoreActions().editor.createNew(descriptor);
+    a.setState(idleState);
+  }),
 
   rejectOffer: thunk((a) => {
     a.setState(idleState);
