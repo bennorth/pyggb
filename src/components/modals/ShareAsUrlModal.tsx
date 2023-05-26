0c5c78b015db9b1198f3a18d55ddbe89569d835c
Ben North <ben@redfrontdoor.org>
2023-05-26 10:48:32 +0100

    BodyComputing: Define local component

---
+++ src/components/modals/ShareAsUrlModal.tsx
@@ -0,0 +1,9 @@
+import { Spinner } from "react-bootstrap";
+
+const BodyComputing = () => {
+  return (
+    <div className="ShareAsUrlModalBody computing">
+      <Spinner></Spinner>
+    </div>
+  );
+};
