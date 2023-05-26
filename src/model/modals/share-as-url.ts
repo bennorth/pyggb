bfc16537a23ba2faeb8ad73fffd965694df36b0d
Ben North <ben@redfrontdoor.org>
2023-05-26 10:35:12 +0100

    zlibCompress(): Add async helper

--- src/model/modals/share-as-url.ts
+++ src/model/modals/share-as-url.ts
@@ -1,6 +1,23 @@
+import { AsyncZlibOptions, zlib } from "fflate";
+
 type ShareAsUrlState =
   | { kind: "idle" }
   | { kind: "computing" }
   | { kind: "ready"; url: string };
 
 const idleState: ShareAsUrlState = { kind: "idle" };
+
+function zlibCompress(
+  data: Uint8Array,
+  opts: AsyncZlibOptions
+): Promise<Uint8Array> {
+  return new Promise<Uint8Array>((resolve, reject) => {
+    zlib(data, opts, (err, data) => {
+      if (err != null) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
