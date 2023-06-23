8c30f7520eff83f5cde587508b657e3d25a7240c
Ben North <ben@redfrontdoor.org>
2023-06-23 10:34:55 +0100

    Update "can create and use URL for project" test

--- cypress/e2e/share-as-url.cy.ts
+++ cypress/e2e/share-as-url.cy.ts
@@ -5,57 +5,49 @@ describe("Share as URL", () => {
   it("can create and use URL for project", () => {
     cy.visit("/");
 
+    const uniqueMarker = uuidv4();
+    const pythonOutput = `hello world ${uniqueMarker}`;
+
     // Include some non-ASCII characters:
-    const filename = `😀😀😀 ${uuidv4()}`;
+    const filename = `😀😀😀 ${uniqueMarker}`;
     createNewPyGgbFile(filename);
 
     const code = `
       # Some non-ASCII: Ⅱ—Ⅳ
-      # And a long line which should end up being trimmed blah blah
       A = Point(3, 4)
       B = Point(5, 2)
       k = Line(A, B)
+      print("${pythonOutput}")
     `;
 
     cy.window().then((window) => {
       window["PYGGB_CYPRESS"].ACE_EDITOR.setValue(deIndent(code));
       chooseFileMenuEntry("Share as link");
 
-      // Can't test the COPY button actually copies because we don'have
-      // access to the clipboard.  But we can get the url out of the
-      // <input> elt.
-      cy.get("input").then(($input) => {
-        const shareUrl = $input[0].value;
+      // Can't test the COPY button actually copies because we don't
+      // have access to the clipboard.  But we can get the url out of
+      // the relevant <input> elt.
+      cy.get("input.shareUrl").then(($input) => {
+        const shareUrl = ($input[0] as HTMLInputElement).value;
 
         // Flush project so we can tell whether project-from-URL has
         // happened or not.
-        createNewPyGgbFile(filename);
-
-        cy.visit(shareUrl);
-        cy.get(".modal-body").contains(
-          `Create file ${filename.substring(0, 10)}`
-        );
-        cy.get("button").contains("Cancel").click();
-        cy.get("div.modal").should("not.exist");
-        cy.get(".editor .busy-overlay").should("not.be.visible");
-
+        createNewPyGgbFile(`SECOND ${filename}`);
         cy.window().then((window) => {
           expect(window["PYGGB_CYPRESS"].ACE_EDITOR.getValue()).contains(
             "writing your code"
           );
 
           cy.visit(shareUrl);
-          cy.get(".modal-body").contains(
-            `Create file ${filename.substring(0, 10)}`
-          );
-          cy.get("button").contains("Create").click();
-          cy.get("div.modal").should("not.exist");
+          // Should have auto-run:
+          cy.get(".stdout-inner").contains(pythonOutput);
+
           cy.get(".editor .busy-overlay").should("not.be.visible");
 
           cy.window().then((window) => {
-            expect(window["PYGGB_CYPRESS"].ACE_EDITOR.getValue()).contains(
-              "Some non-ASCII: Ⅱ—Ⅳ"
-            );
+            const codeText = window["PYGGB_CYPRESS"].ACE_EDITOR.getValue();
+            expect(codeText).contains("Some non-ASCII: Ⅱ—Ⅳ");
+            expect(codeText).contains(uniqueMarker);
           });
         });
       });
