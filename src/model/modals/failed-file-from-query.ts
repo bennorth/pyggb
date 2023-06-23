aede973ca413d5336576cb3a314be0d3e0557851
Ben North <ben@redfrontdoor.org>
2023-06-23 10:30:47 +0100

    kIdleFailedFileFromQueryState: Define constant

--- src/model/modals/failed-file-from-query.ts
+++ src/model/modals/failed-file-from-query.ts
@@ -1,3 +1,5 @@
 type FailedFileFromQueryState =
   | { kind: "idle" }
   | { kind: "awaiting-user-acknowledgment"; message: string };
+
+const kIdleFailedFileFromQueryState = { kind: "idle" } as const;
