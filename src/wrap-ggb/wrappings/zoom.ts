d8d6dbed528ec9fa20fe964d4e61c096a5c1a4dc
Ben North <ben@redfrontdoor.org>
2023-11-10 12:11:58 +0000

    Define bad-args error and helper

--- src/wrap-ggb/wrappings/zoom.ts
+++ src/wrap-ggb/wrappings/zoom.ts
@@ -11,6 +11,19 @@ export const register = (mod: any, appApi: AppApi) => {
   const ggb = augmentedGgbApi(appApi.ggb);
 
   const zoomIn = new Sk.builtin.func((...args) => {
+    const badArgsError = new Sk.builtin.TypeError(
+      "ZoomIn() arguments must be: empty (to reset to default);" +
+        " a single number (scale factor);" +
+        " a number (scale factor) and a point (zoom centre);" +
+        " a number (scale factor) and a 2-element tuple or list (zoom centre);" +
+        " or four numbers (min-x, min-y, max-x, max-y)"
+    );
+    function throwBadArgsUnless(argsAreOk: boolean): asserts argsAreOk is true {
+      if (!argsAreOk) {
+        throw badArgsError;
+      }
+    }
+
     return Sk.builtin.none.none$;
   });
 
