a7b0f698566e0d5cf0f81f6b9070f10214c2a4ed
Ben North <ben@redfrontdoor.org>
2024-01-27 08:15:41 +0000

    Handle 3-arg case

--- src/wrap-ggb/wrappings/rotate.ts
+++ src/wrap-ggb/wrappings/rotate.ts
@@ -29,6 +29,23 @@ export const register = (mod: any, appApi: AppApi) => {
         const label = ggb.evalCmd(ggbCmd);
         return ggb.wrapExistingGgbObject(label);
       }
+      case 3: {
+        if (
+          !ggb.isGgbObject(args[0]) ||
+          !ggb.isGgbObjectOfType(args[2], "point")
+        ) {
+          throw badArgsError;
+        }
+
+        const pyAngle = args[1];
+        ggb.throwIfNotPyOrGgbNumber(pyAngle, "rotation angle");
+        const angleArg = ggb.numberValueOrLabel(pyAngle);
+
+        const ggbArgs = [args[0].$ggbLabel, angleArg, args[2].$ggbLabel];
+        const ggbCmd = assembledCommand("Rotate", ggbArgs);
+        const label = ggb.evalCmd(ggbCmd);
+        return ggb.wrapExistingGgbObject(label);
+      }
       default:
         throw badArgsError;
     }
