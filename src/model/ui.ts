33964c7a5e886bd49c0bb5e6baaae5c1a46ef279
Ben North <ben.north@tcd.ie>
2024-10-09 11:05:13 +0100

    uiSettings: Define model slice value

--- src/model/ui.ts
+++ src/model/ui.ts
@@ -1,8 +1,14 @@
 import { Action } from "easy-peasy";
+import { propSetterAction } from "../shared/utils";
 
 export type UiLayout = "full" | "ggb-construction-only";
 
 export type UiSettings = {
   uiLayout: UiLayout;
   setUiLayout: Action<UiSettings, UiLayout>;
+};
+
+export let uiSettings: UiSettings = {
+  uiLayout: "full",
+  setUiLayout: propSetterAction("uiLayout"),
 };
