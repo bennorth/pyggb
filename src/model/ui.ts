2233fda859b67e8271cbbe237db6563fbfc768f5
Ben North <ben.north@tcd.ie>
2024-10-09 11:04:56 +0100

    UiSettings: Define model slice type

--- src/model/ui.ts
+++ src/model/ui.ts
@@ -1,1 +1,8 @@
+import { Action } from "easy-peasy";
+
 export type UiLayout = "full" | "ggb-construction-only";
+
+export type UiSettings = {
+  uiLayout: UiLayout;
+  setUiLayout: Action<UiSettings, UiLayout>;
+};
