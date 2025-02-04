a36f9f31e55796310097daf72046ccfbdda2660b
Ben North <ben@redfrontdoor.org>
2025-02-04 20:49:09 +0000

    User settle functions: Define types, constant

--- src/model/modals/download-file.ts
+++ src/model/modals/download-file.ts
@@ -2,3 +2,7 @@ type DownloadFileLaunchArgs<ContentT> = {
   suggestedFileName: string;
   content: ContentT;
 };
+
+type UserSettleResult = "cancel" | "submit";
+type UserSettleFun = (result: UserSettleResult) => void;
+const kIgnoreSettleResult: UserSettleFun = () => void 0;
