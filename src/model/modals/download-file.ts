f850f5b90c3b4fd8967f9987cc12ce9910088e64
Ben North <ben@redfrontdoor.org>
2025-02-04 20:49:44 +0000

    DownloadFsmState: Define type, "idle" constant

--- src/model/modals/download-file.ts
+++ src/model/modals/download-file.ts
@@ -6,3 +6,9 @@ type DownloadFileLaunchArgs<ContentT> = {
 type UserSettleResult = "cancel" | "submit";
 type UserSettleFun = (result: UserSettleResult) => void;
 const kIgnoreSettleResult: UserSettleFun = () => void 0;
+
+export type DownloadFsmState =
+  | { kind: "idle" }
+  | { kind: "interacting"; userSettle: UserSettleFun };
+
+const kFsmStateIdle: DownloadFsmState = { kind: "idle" };
