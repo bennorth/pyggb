6aafae6b511eb7c68bc5f3193040d58cf2422af7
Ben North <ben@redfrontdoor.org>
2025-02-04 20:54:47 +0000

    arrayBufferFromStr(): Define helper

--- src/model/download-as-filetype.ts
+++ src/model/download-as-filetype.ts
@@ -9,3 +9,12 @@ export type DownloadAsFiletype = {
   downloadPy: DownloadFile<string>;
   downloadGgb: DownloadFile<ArrayBuffer>;
 };
+
+function arrayBufferFromStr(str: string): ArrayBuffer {
+  var buffer = new ArrayBuffer(str.length);
+  var dataView = new DataView(buffer);
+  for (var i = 0; i < str.length; ++i) {
+    dataView.setUint8(i, str.charCodeAt(i));
+  }
+  return buffer;
+}
