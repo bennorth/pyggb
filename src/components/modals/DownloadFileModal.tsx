dfc2aaf6d8635158f15a7ac5d0be555d240edf9f
Ben North <ben@redfrontdoor.org>
2025-02-04 21:05:38 +0000

    DownloadFileModal: Compute state, actions, helpers

--- src/components/modals/DownloadFileModal.tsx
+++ src/components/modals/DownloadFileModal.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { KeyboardEvent } from "react";
 import { PyGgbModel } from "../../model";
 import { DownloadFsmState } from "../../model/modals/download-file";
 import { Actions, State } from "easy-peasy";
+import { useStoreActions, useStoreState } from "../../store";
 
 type DownloadFileModalProps = {
   fileTypeDisplayName: string;
@@ -15,4 +16,25 @@ export const DownloadFileModal: React.FC<DownloadFileModalProps> = ({
   selectFilename,
   selectSetFilename,
 }) => {
+  const fsmState = useStoreState(selectFsmState);
+  const filename = useStoreState(selectFilename);
+  const setFilename = useStoreActions(selectSetFilename);
+
+  if (fsmState.kind === "idle") {
+    return null;
+  }
+
+  const settle = fsmState.userSettle;
+
+  const downloadEnabled = filename !== "";
+
+  const onKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    console.log(e);
+    if (e.key === "Enter") {
+      if (downloadEnabled) {
+        settle("submit");
+      }
+      e.preventDefault();
+    }
+  };
 };
