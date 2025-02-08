68a362addfc4da0804d099b95d43f4c698246fea
Ben North <ben@redfrontdoor.org>
2025-02-08 15:44:06 +0000

    uncompressedShareSpec(): Extract e2e helper

--- cypress/e2e/share-as-url.cy.ts
+++ cypress/e2e/share-as-url.cy.ts
@@ -14,7 +14,7 @@ describe("Share as URL", () => {
     `);
   };
 
-  it("can use URL with uncompressed code", () => {
+  const uncompressedShareSpec = () => {
     const uniqueMarker = uuidv4();
     const pythonOutput = `hello world ${uniqueMarker}`;
     const code = codeWithMarker(uniqueMarker);
@@ -23,7 +23,11 @@ describe("Share as URL", () => {
     const pCode = b64StringFromBinaryString(bstrCode);
 
     const shareUrl = `/?name=Testing123&code=${pCode}&cck=none`;
+    return { pythonOutput, shareUrl };
+  };
 
+  it("can use URL with uncompressed code", () => {
+    const { pythonOutput, shareUrl } = uncompressedShareSpec();
     cy.visit(shareUrl);
     cy.get(".stdout-inner").contains(pythonOutput);
   });
