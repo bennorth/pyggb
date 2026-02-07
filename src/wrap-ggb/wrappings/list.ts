e8eeaac4bc0edae36d17a4a859f84a610b98dc78
Ben North <ben@redfrontdoor.org>
2026-02-07 10:14:51 +0000

    Define constructor spec type, function

--- src/wrap-ggb/wrappings/list.ts
+++ src/wrap-ggb/wrappings/list.ts
@@ -2,6 +2,7 @@ import { RegisterFun } from "../../shared/appApi";
 import { augmentedGgbApi, SkGgbObject, WrapExistingCtorSpec } from "../shared";
 import { SkulptApi } from "../../shared/vendor-types/skulptapi";
 import { registerObjectType } from "../type-registry";
+import { throwBadSpecKind } from "../../shared/utils";
 
 declare var Sk: SkulptApi; // eslint-disable-line no-var
 
@@ -11,12 +12,33 @@ interface SkGgbList extends SkGgbObject {
 
 type SkGgbListCtorSpec =
   | WrapExistingCtorSpec
-  /* TODO */ ;
+  | { kind: "empty" }
+  | { kind: "iterable"; elements: Array<SkGgbObject> };
 
 export const register: RegisterFun = (mod, appApi) => {
   const ggb = augmentedGgbApi(appApi.ggb);
 
   const cls = Sk.abstr.buildNativeClass("List", {
+    constructor: function List(this: SkGgbList, spec: SkGgbListCtorSpec) {
+      switch (spec.kind) {
+        case "wrap-existing": {
+          this.$ggbLabel = spec.label;
+          break;
+        }
+        case "empty": {
+          this.$ggbLabel = ggb.evalCmd("{}");
+          break;
+        }
+        case "iterable": {
+          const elementLabels = spec.elements.map((elt) => elt.$ggbLabel);
+          const ggbCmd = `{${elementLabels.join(",")}}`;
+          this.$ggbLabel = ggb.evalCmd(ggbCmd);
+          break;
+        }
+        default:
+          throwBadSpecKind("List", spec);
+      }
+    },
     slots: {
     },
   });
