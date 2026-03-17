47de153c7b753002735f3d5a3b54c11203bc63f4
Ben North <ben@redfrontdoor.org>
2026-03-17 17:33:39 +0000

    Perimeter(): Simplify tests

--- cypress/python-programs/perimeter.ts
+++ cypress/python-programs/perimeter.ts
@@ -1,3 +1,5 @@
+import { newPolygonSpecs } from "./polygon-specs";
+
 const conicSpecs = [
   {
     label: "Perimeter(circle)",
@@ -20,55 +22,17 @@ const conicSpecs = [
   },
 ];
 
-// Not entirely clear that the following is better than just writing all
-// the cases out, but anyway.
+function polygonPerimeter(points: Array<Array<number>>): number {
+  const nPoints = points.length;
+  let perimeter = 0.0;
+  for (let i = 0; i < nPoints; ++i) {
+    const p0 = points[i];
+    const p1 = points[(i + 1) % nPoints];
+    perimeter += Math.hypot(p1[0] - p0[0], p1[1] - p0[1]);
+  }
+  return perimeter;
+}
 
-const kOctagonVertices = [
-  [1, 0],
-  [2, 0],
-  [3, 1],
-  [3, 2],
-  [2, 3],
-  [1, 3],
-  [0, 2],
-  [0, 1],
-];
-
-const polygonSpecs = [3, 4, 5, 6, 7, 8].map((nSides) => {
-  const range = Array.from({ length: nSides }).map((_, idx) => idx);
-
-  const pointDefns = range
-    .map((idx) => {
-      const point = kOctagonVertices[idx];
-      return `  p${idx} = Point(${point[0]}, ${point[1]})`;
-    })
-    .join("\n");
-
-  const argsStr = range.map((idx) => `p${idx}`).join(",");
-  const tailCode = [
-    `  k = Polygon([${argsStr}])`,
-    "  d = Perimeter(k).value",
-    "  print('%.0f' % (d * 100000))",
-    "  ",
-  ].join("\n");
-
-  const code = `\n${pointDefns}\n${tailCode}`;
-
-  const expPerimeter = range
-    .map((idx) => {
-      const p0 = kOctagonVertices[idx];
-      const p1 = kOctagonVertices[(idx + 1) % nSides];
-      return Math.hypot(p1[0] - p0[0], p1[1] - p0[1]);
-    })
-    .reduce((a, b) => a + b, 0.0);
-
-  const expScaledPerimeterStr = Math.round(100000 * expPerimeter).toString();
-
-  return {
-    label: `Perimeter (${nSides}-gon)`,
-    code,
-    expOutputs: [expScaledPerimeterStr],
-  };
-});
+const polygonSpecs = newPolygonSpecs("Perimeter", polygonPerimeter);
 
 export const specs = [...conicSpecs, ...polygonSpecs];
