e191bf62a97193dc4f44edf44d83bf35b7f3d6a1
Ben North <ben.north@tcd.ie>
2026-03-29 18:47:57 +0100

    numberArgStrings(): Define helper

--- src/wrap-ggb/wrappings/function-graph.ts
+++ src/wrap-ggb/wrappings/function-graph.ts
@@ -7,4 +7,14 @@ declare var Sk: SkulptApi; // eslint-disable-line no-var
 
 export const register: RegisterFun = (mod, appApi) => {
   const ggb = augmentedGgbApi(appApi.ggb);
+
+  function numberArgStrings(
+    args: Array<SkObject>,
+    errorMessage: string
+  ): Array<string> {
+    if (!args.every(ggb.isPythonOrGgbNumber)) {
+      throw new Sk.builtin.TypeError(errorMessage);
+    }
+    return args.map(ggb.numberValueOrLabel);
+  }
 };
