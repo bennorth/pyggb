a373043fbe19103253f391c396b93b2d28fd13f8
Ben North <ben@redfrontdoor.org>
2026-04-04 12:25:13 +0100

    Add skeleton file

---
+++ src/wrap-ggb/coords.ts
@@ -0,0 +1,5 @@
+import { GgbApi } from "../shared/vendor-types/ggbapi";
+import { SkObject, SkulptApi } from "../shared/vendor-types/skulptapi";
+import { ReadWriteProperty, SkGgbObject, throwIfNotNumber } from "./shared";
+
+declare var Sk: SkulptApi; // eslint-disable-line no-var
