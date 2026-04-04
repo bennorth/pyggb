cfca39e67719935c739acdb4a67c602106824bc5
Ben North <ben@redfrontdoor.org>
2026-04-04 12:25:18 +0100

    CoordinateProtoSlice: Define type

--- src/wrap-ggb/coords.ts
+++ src/wrap-ggb/coords.ts
@@ -3,3 +3,10 @@ import { SkObject, SkulptApi } from "../shared/vendor-types/skulptapi";
 import { ReadWriteProperty, SkGgbObject, throwIfNotNumber } from "./shared";
 
 declare var Sk: SkulptApi; // eslint-disable-line no-var
+
+export type CoordinateProtoSlice = {
+  $xCoord(this: CoordinateProtoSlice): number;
+  $setXCoord(this: CoordinateProtoSlice, x: number): void;
+  $yCoord(this: CoordinateProtoSlice): number;
+  $setYCoord(this: CoordinateProtoSlice, y: number): void;
+};
