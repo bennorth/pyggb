4eef887d35735a0769de89cf526538ec0a1276eb
Ben North <ben@redfrontdoor.org>
2026-04-06 15:06:56 +0100

    constructInstanceFun(): Simplify

--- src/wrap-ggb/command-invocation.ts
+++ src/wrap-ggb/command-invocation.ts
@@ -349,12 +349,9 @@ function constructInstanceFun(nativeClass: {
     const labelsStr = evalInfo.maybeLabels;
     const labels = labelsStr.split(",");
 
-    const nLabels = labelsStr === "" ? 0 : labels.length;
-    if (nLabels !== 1 && !takeFirst)
-      throw new Sk.builtin.RuntimeError(
-        `expecting one result from ${evalInfo.commandName}()` +
-          ` but got ${nLabels}`
-      );
+    if (!takeFirst) {
+      verifyExactlyOneLabel(labelsStr, evalInfo.commandName);
+    }
 
     const label = takeFirst ? labels[0] : labelsStr;
     return new nativeClass(label);
