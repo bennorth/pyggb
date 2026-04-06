bb36cf51d7176c19aa98008351631efb9b204995
Ben North <ben@redfrontdoor.org>
2026-04-06 09:55:41 +0100

    Add skeleton "command invocation" file

---
+++ src/wrap-ggb/command-invocation.ts
@@ -0,0 +1,4 @@
+import { GgbApi } from "../shared/vendor-types/ggbapi";
+import { SkObject, SkulptApi } from "../shared/vendor-types/skulptapi";
+
+declare var Sk: SkulptApi; // eslint-disable-line no-var
