c8d354dab8e86baa3f7351fd8e845219ae571fad
Ben North <ben@redfrontdoor.org>
2026-04-06 10:29:31 +0100

    Incircle(): Simplify

--- src/wrap-ggb/wrappings/incircle.ts
+++ src/wrap-ggb/wrappings/incircle.ts
@@ -1,33 +1,15 @@
 import { RegisterFun } from "../../shared/appApi";
-import { augmentedGgbApi, AugmentedGgbApi } from "../shared";
 import { SkulptApi } from "../../shared/vendor-types/skulptapi";
+import { SignatureSpec, wrapIfMatching } from "../command-invocation";
 
 declare var Sk: SkulptApi; // eslint-disable-line no-var
 
+const kSignatures: Array<SignatureSpec> = [
+  { argTypes: ["point", "point", "point"] },
+];
+
 export const register: RegisterFun = (mod, appApi) => {
-  const ggb: AugmentedGgbApi = augmentedGgbApi(appApi.ggb);
-
-  const fun = new Sk.builtin.func((...args) => {
-    const badArgsError = new Sk.builtin.TypeError(
-      "Incircle() arguments must be (point, point, point)"
-    );
-
-    switch (args.length) {
-      case 3: {
-        if (!ggb.everyElementIsGgbObjectOfType(args, "point")) {
-          throw badArgsError;
-        }
-
-        const ggbArgs = args.map((a) => a.$ggbLabel);
-        const ggbArgStr = ggbArgs.join(",");
-        const ggbCmd = `Incircle(${ggbArgStr})`;
-        const label = ggb.evalCmd(ggbCmd);
-        return ggb.wrapExistingGgbObject(label);
-      }
-      default:
-        throw badArgsError;
-    }
-  });
-
-  mod.Incircle = fun;
+  mod.Incircle = new Sk.builtin.func((...args) =>
+    wrapIfMatching(appApi.ggb, kSignatures, "Incircle", args)
+  );
 };
