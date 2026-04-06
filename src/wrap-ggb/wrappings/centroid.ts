36b75b63b7687d04b65c0ecbf5e7214ad803c37e
Ben North <ben@redfrontdoor.org>
2026-04-06 10:26:03 +0100

    Centroid(): Simplify

--- src/wrap-ggb/wrappings/centroid.ts
+++ src/wrap-ggb/wrappings/centroid.ts
@@ -1,32 +1,16 @@
 import { RegisterFun } from "../../shared/appApi";
-import { augmentedGgbApi, AugmentedGgbApi } from "../shared";
+import { kPolygonTypeAndSubtypes } from "../shared";
 import { SkulptApi } from "../../shared/vendor-types/skulptapi";
+import { SignatureSpec, wrapIfMatching } from "../command-invocation";
 
 declare var Sk: SkulptApi; // eslint-disable-line no-var
 
+const kSignatures: Array<SignatureSpec> = [
+  { argTypes: [kPolygonTypeAndSubtypes] },
+];
+
 export const register: RegisterFun = (mod, appApi) => {
-  const ggb: AugmentedGgbApi = augmentedGgbApi(appApi.ggb);
-
-  const fun = new Sk.builtin.func((...args) => {
-    const badArgsError = new Sk.builtin.TypeError(
-      "Centroid() arguments must be (polygon)"
-    );
-
-    switch (args.length) {
-      case 1: {
-        const arg = args[0];
-        if (!ggb.isGgbObjectOfType(arg, "polygon")) {
-          throw badArgsError;
-        }
-
-        const ggbCmd = `Centroid(${arg.$ggbLabel})`;
-        const label = ggb.evalCmd(ggbCmd);
-        return ggb.wrapExistingGgbObject(label);
-      }
-      default:
-        throw badArgsError;
-    }
-  });
-
-  mod.Centroid = fun;
+  mod.Centroid = new Sk.builtin.func((...args) =>
+    wrapIfMatching(appApi.ggb, kSignatures, "Centroid", args)
+  );
 };
