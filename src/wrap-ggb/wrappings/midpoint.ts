2b594d8c587e482f054f4182c3c088ea3f71cfa9
Ben North <ben@redfrontdoor.org>
2026-04-06 10:32:24 +0100

    Midpoint(): Simplify

--- src/wrap-ggb/wrappings/midpoint.ts
+++ src/wrap-ggb/wrappings/midpoint.ts
@@ -1,38 +1,16 @@
 import { RegisterFun } from "../../shared/appApi";
-import { augmentedGgbApi, AugmentedGgbApi } from "../shared";
 import { SkulptApi } from "../../shared/vendor-types/skulptapi";
+import { SignatureSpec, wrapIfMatching } from "../command-invocation";
 
 declare var Sk: SkulptApi; // eslint-disable-line no-var
 
-const kArgTypes = ["segment", "circle", "ellipse"];
+const kSignatures: Array<SignatureSpec> = [
+  { argTypes: [["segment", "circle", "ellipse"]] },
+  { argTypes: ["point", "point"] },
+];
 
 export const register: RegisterFun = (mod, appApi) => {
-  const ggb: AugmentedGgbApi = augmentedGgbApi(appApi.ggb);
-
-  const fun = new Sk.builtin.func((...args) => {
-    const badArgsError = new Sk.builtin.TypeError(
-      "Midpoint() arguments must be" +
-        " (segment), or (circle), or (ellipse), or (point, point)"
-    );
-
-    switch (args.length) {
-      case 1: {
-        const arg = args[0];
-        if (!ggb.isGgbObjectOfSomeType(arg, kArgTypes)) {
-          throw badArgsError;
-        }
-        return ggb.existingFromCmdAndGgbArgs("Midpoint", [arg]);
-      }
-      case 2: {
-        if (!ggb.everyElementIsGgbObjectOfType(args, "point")) {
-          throw badArgsError;
-        }
-        return ggb.existingFromCmdAndGgbArgs("Midpoint", args);
-      }
-      default:
-        throw badArgsError;
-    }
-  });
-
-  mod.Midpoint = fun;
+  mod.Midpoint = new Sk.builtin.func((...args) =>
+    wrapIfMatching(appApi.ggb, kSignatures, "Midpoint", args)
+  );
 };
