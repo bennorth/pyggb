6db1e1c93bef91e42171b12069801bc77a468c3c
Ben North <ben@redfrontdoor.org>
2026-04-06 10:39:11 +0100

    Rotate(): Simplify

--- src/wrap-ggb/wrappings/rotate.ts
+++ src/wrap-ggb/wrappings/rotate.ts
@@ -1,48 +1,16 @@
 import { RegisterFun } from "../../shared/appApi";
-import { augmentedGgbApi, assembledCommand, AugmentedGgbApi } from "../shared";
 import { SkulptApi } from "../../shared/vendor-types/skulptapi";
+import { SignatureSpec, wrapIfMatching } from "../command-invocation";
 
 declare var Sk: SkulptApi; // eslint-disable-line no-var
 
+const kSignatures: Array<SignatureSpec> = [
+  { argTypes: ["ggb-object", ["either-number", "angle"]] },
+  { argTypes: ["ggb-object", ["either-number", "angle"], "point"] },
+];
+
 export const register: RegisterFun = (mod, appApi) => {
-  const ggb: AugmentedGgbApi = augmentedGgbApi(appApi.ggb);
-
-  const fun = new Sk.builtin.func((...args) => {
-    const badArgsError = new Sk.builtin.TypeError(
-      "Rotate() arguments must be" +
-        " (object, angle)" +
-        " or (object, angle, rotation_center_point)"
-    );
-
-    const ggbRotate = (extraArgs: Array<string>) => {
-      if (!ggb.isGgbObject(args[0])) {
-        throw badArgsError;
-      }
-
-      const pyAngle = args[1];
-      ggb.throwIfNotPyOrGgbNumber(pyAngle, "rotation angle");
-      const angleArg = ggb.numberValueOrLabel(pyAngle);
-
-      const ggbArgs = [args[0].$ggbLabel, angleArg, ...extraArgs];
-      const ggbCmd = assembledCommand("Rotate", ggbArgs);
-      const label = ggb.evalCmd(ggbCmd);
-      return ggb.wrapExistingGgbObject(label);
-    };
-
-    switch (args.length) {
-      case 2: {
-        return ggbRotate([]);
-      }
-      case 3: {
-        if (!ggb.isGgbObjectOfType(args[2], "point")) {
-          throw badArgsError;
-        }
-        return ggbRotate([args[2].$ggbLabel]);
-      }
-      default:
-        throw badArgsError;
-    }
-  });
-
-  mod.Rotate = fun;
+  mod.Rotate = new Sk.builtin.func((...args) =>
+    wrapIfMatching(appApi.ggb, kSignatures, "Rotate", args)
+  );
 };
