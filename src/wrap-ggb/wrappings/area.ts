a820a2cc18c0f7e3ccc7f7e797e6534e3693d8a1
Ben North <ben@redfrontdoor.org>
2026-04-06 10:25:10 +0100

    Area(): Simplify

--- src/wrap-ggb/wrappings/area.ts
+++ src/wrap-ggb/wrappings/area.ts
@@ -1,43 +1,17 @@
 import { RegisterFun } from "../../shared/appApi";
-import {
-  AugmentedGgbApi,
-  augmentedGgbApi,
-  kPolygonTypeAndSubtypes,
-} from "../shared";
+import { kPolygonTypeAndSubtypes } from "../shared";
 import { SkulptApi } from "../../shared/vendor-types/skulptapi";
+import { SignatureSpec, wrapIfMatching } from "../command-invocation";
 
 declare var Sk: SkulptApi; // eslint-disable-line no-var
 
-const kArgTypes = ["circle", "ellipse", ...kPolygonTypeAndSubtypes];
+const kSignatures: Array<SignatureSpec> = [
+  { argTypes: [["circle", "ellipse", ...kPolygonTypeAndSubtypes]] },
+  { argTypes: { atLeast: 3, ofType: "point" } },
+];
 
 export const register: RegisterFun = (mod, appApi) => {
-  const ggb: AugmentedGgbApi = augmentedGgbApi(appApi.ggb);
-
-  const fun = new Sk.builtin.func((...args) => {
-    const badArgsError = new Sk.builtin.TypeError(
-      "Area() arguments must be (circle), or (ellipse), or (polygon)," +
-        " or (point1, point2, point3, ...) with at least three points"
-    );
-
-    switch (args.length) {
-      case 0:
-        throw badArgsError;
-      case 1: {
-        const arg = args[0];
-        if (!ggb.isGgbObjectOfSomeType(arg, kArgTypes)) {
-          throw badArgsError;
-        }
-        return ggb.existingFromCmdAndGgbArgs("Area", [arg]);
-      }
-      case 2:
-        throw badArgsError;
-      default:
-        if (!ggb.everyElementIsGgbObjectOfType(args, "point")) {
-          throw badArgsError;
-        }
-        return ggb.existingFromCmdAndGgbArgs("Area", args);
-    }
-  });
-
-  mod.Area = fun;
+  mod.Area = new Sk.builtin.func((...args) =>
+    wrapIfMatching(appApi.ggb, kSignatures, "Area", args)
+  );
 };
